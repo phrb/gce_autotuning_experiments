e3e1b525b36b98cb697ade1242c166416c7de247
phrb <pedro.bruel@gmail.com>
2015-11-19 19:58:02 -0200

    Changes to submodules.

--- tsp/adddeps.py
+++ tsp/adddeps.py
@@ -6,5 +6,4 @@ path = "../../opentuner/opentuner/utils/adddeps.py"
 target = os.path.join(os.path.dirname(__file__), path)
 
 if os.path.isfile(target):
-    print "was a path"
     execfile(target, dict(__file__=target))
